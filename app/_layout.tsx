538ff4b8d6de42812e997e6816600bf2081ad2af
Kyung-A <nek1717@naver.com>
2026-04-02 22:27:35 +0900

    feat: DB 테이블 쿼리문 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
 import React from "react";
 
+import { initDB } from "@/lib/db";
+import { SQLiteProvider } from "expo-sqlite";
 import "react-native-reanimated";
 import "../global.css";
 
@@ -10,28 +12,30 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShadowVisible: false,
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="create" options={{ headerShown: false }} />
-      <Stack.Screen name="detail" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="edit"
-        options={{
-          presentation: "modal",
-          headerShown: false,
+    <SQLiteProvider databaseName="logplan.db" onInit={initDB}>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
         }}
-      />
-      <Stack.Screen
-        name="sub-detail"
-        options={{
-          presentation: "modal",
-          headerShown: false,
-        }}
-      />
-    </Stack>
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="create" options={{ headerShown: false }} />
+        <Stack.Screen name="detail" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="edit"
+          options={{
+            presentation: "modal",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sub-detail"
+          options={{
+            presentation: "modal",
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </SQLiteProvider>
   );
 }
