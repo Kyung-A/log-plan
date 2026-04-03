c97113f3bdf2661d7f157a15ea5c637c1ce9d7bb
Kyung-A <nek1717@naver.com>
2026-04-03 16:53:48 +0900

    feat: 목록 조회 로직 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Goal } from "@/components/Goal";
 import { GoalTask } from "@/components/GoalTask";
 import { IGoal } from "@/types/goal";
+import { ITask } from "@/types/task";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { router } from "expo-router";
-import { useState } from "react";
+import { router, useFocusEffect } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+import { useCallback, useState } from "react";
 import {
   FlatList,
   Pressable,
@@ -13,20 +15,83 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const query = `
+        SELECT 
+            g.id,
+            g.title,
+            g.end_date,
+            g.is_completed,
+            -- 모든 세부 계획의 현재 횟수 총합
+            IFNULL(SUM(t.current_count), 0) AS total_current,
+            -- 모든 세부 계획의 목표 횟수 총합
+            IFNULL(SUM(t.target_count), 0) AS total_target,
+            -- 달성률 계산
+            CASE 
+                WHEN IFNULL(SUM(t.target_count), 0) > 0 
+                THEN ROUND(CAST(SUM(t.current_count) AS FLOAT) / SUM(t.target_count) * 100, 1)
+                ELSE 0 
+            END AS achievement_rate
+        FROM goals g
+        LEFT JOIN tasks t ON g.id = t.goal_id
+        GROUP BY g.id
+        ORDER BY g.created_at DESC;
+    `;
+
+const renderSection = ({
+  item,
+  expandedSections,
+  setExpandedSections,
+}: {
+  item: IGoal;
+  expandedSections: string[];
+  setExpandedSections: React.Dispatch<React.SetStateAction<string[]>>;
+}) => {
+  const isExpanded = expandedSections.includes(item.id);
+
+  return (
+    <View className="w-full mb-2">
+      <Goal data={item} setExpandedSections={setExpandedSections} />
+      {isExpanded && <GoalTask task={item.tasks} />}
+    </View>
+  );
+};
+
 export default function HomeScreen() {
+  const db = useSQLiteContext();
+
+  const [goals, setGoals] = useState<IGoal[]>([]);
   const [expandedSections, setExpandedSections] = useState<string[]>([]);
 
-  const renderSection = ({ item }: { item: IGoal }) => {
-    const isExpanded = expandedSections.includes(item.id);
+  const loadData = async (isActive: boolean) => {
+    try {
+      const goals = await db.getAllAsync<IGoal>(query);
+      const tasks = await db.getAllAsync<ITask>(
+        "SELECT * FROM tasks ORDER BY id ASC",
+      );
+      const result = goals.map((goal) => ({
+        ...goal,
+        tasks: tasks.filter((task) => task.goal_id === goal.id),
+      }));
 
-    return (
-      <View className="w-full mb-2">
-        <Goal data={item} setExpandedSections={setExpandedSections} />
-
-        {isExpanded && <GoalTask task={item.tasks} />}
-      </View>
-    );
+      if (isActive) {
+        setGoals(result);
+      }
+    } catch (error) {
+      console.error("데이터 조회 실패:", error);
+    }
   };
+
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+
+      loadData(isActive);
+
+      return () => {
+        isActive = false;
+      };
+    }, [db]),
+  );
 
   return (
     <SafeAreaView className="flex-1 bg-beige" edges={["top", "left", "right"]}>
@@ -50,9 +115,12 @@ export default function HomeScreen() {
         </View>
 
         <FlatList
-          data={[]}
-          renderItem={renderSection}
-          keyExtractor={(item) => item.title}
+          data={goals}
+          renderItem={({ item }) =>
+            renderSection({ item, expandedSections, setExpandedSections })
+          }
+          keyExtractor={(item) => item.id}
+          extraData={expandedSections}
         />
       </View>
     </SafeAreaView>
