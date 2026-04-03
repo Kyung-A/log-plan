4f5f642d26162cf85e94b974c770fe4ee5c94534
Kyung-A <nek1717@naver.com>
2026-04-03 16:54:02 +0900

    feat: root suspense 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import { Stack } from "expo-router";
-import React from "react";
+import React, { Suspense } from "react";
 
 import { initDB } from "@/lib/db";
 import { SQLiteProvider } from "expo-sqlite";
+import { ActivityIndicator, View } from "react-native";
 import "react-native-reanimated";
 import "../global.css";
 
@@ -12,30 +13,38 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <SQLiteProvider databaseName="logplan.db" onInit={initDB}>
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-        }}
-      >
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="create" options={{ headerShown: false }} />
-        <Stack.Screen name="detail" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="edit"
-          options={{
-            presentation: "modal",
-            headerShown: false,
+    <Suspense
+      fallback={
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator />
+        </View>
+      }
+    >
+      <SQLiteProvider databaseName="logplan.db" onInit={initDB} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
           }}
-        />
-        <Stack.Screen
-          name="sub-detail"
-          options={{
-            presentation: "modal",
-            headerShown: false,
-          }}
-        />
-      </Stack>
-    </SQLiteProvider>
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="create" options={{ headerShown: false }} />
+          <Stack.Screen name="detail" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="edit"
+            options={{
+              presentation: "modal",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="sub-detail"
+            options={{
+              presentation: "modal",
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </SQLiteProvider>
+    </Suspense>
   );
 }
